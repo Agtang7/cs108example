cf73eaee61021a6a05e299ad21978c0becec8023
Ahtang7 <agtang@bu.edu>
2022-04-26 18:25:11 -0400

    Final Project

--- quotes/models.py
+++ quotes/models.py
@@ -2,14 +2,3 @@
 
 from django.urls import reverse
 
-class Quote(models.Model):
-    '''Encapsulate the idea of a quote (i.e., text).'''
-
-    # data attributes of a quote:
-    text = models.TextField(blank=True)
-    person = models.ForeignKey('Person', on_delete=models.CASCADE)
-    # ... other methods here 
-
-    def get_absolute_url(self):
-        '''Return a URL to display this quote object.'''
-        return reverse("quote", kwargs={"pk": self.pk})
